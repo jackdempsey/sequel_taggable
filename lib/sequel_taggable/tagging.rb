9226ea68d7c1fd36f288de943ba68ae360d328e6
Jack Dempsey <jack.dempsey@gmail.com>
2008-12-02 17:42:38 -0500

    add in spec for tagging

--- lib/sequel_taggable/tagging.rb
+++ lib/sequel_taggable/tagging.rb
@@ -1,7 +1,9 @@
 class Tagging < Sequel::Model
   many_to_one :tag
+  
+  validates_presence_of :tag_id, :taggable_id, :taggable_type, :tag_context
 
-  # def taggable
-  #   eval("#{taggable_type}.get!(#{taggable_id})") if taggable_type and taggable_id
-  # end
+  def taggable
+    eval("#{taggable_type}.get!(#{taggable_id})") if taggable_type and taggable_id
+  end
 end
