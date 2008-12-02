b8a4461e80ad4dfaa5b0f51328cb54cf0745c438
Jack Dempsey <jack.dempsey@gmail.com>
2008-12-02 16:55:16 -0500

    reorder requires

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,9 @@ $:.push File.join(File.dirname(__FILE__), '..', 'lib')
 require 'rubygems'
 require 'spec'
 require 'sequel'
-require File.dirname(__FILE__) + '/../lib/sequel_taggable'
+
 require File.dirname(__FILE__) + '/sequel-setup'
+require File.dirname(__FILE__) + '/../lib/sequel_taggable'
+
+
 
