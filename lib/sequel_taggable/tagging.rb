d522615807454cb05bf83f0f9dec07aa44b68382
Jack Dempsey <jack.dempsey@gmail.com>
2008-12-07 21:03:14 -0500

    simplify methods to depend on simple polymorphic associations

--- lib/sequel_taggable/tagging.rb
+++ lib/sequel_taggable/tagging.rb
@@ -1,9 +1,11 @@
 class Tagging < Sequel::Model
+  is :polymorphic
   many_to_one :tag
+  many_to_one :taggable, :polymorphic => true
   
-  validates_presence_of :tag_id, :taggable_id, :taggable_type, :tag_context
+  validates_presence_of :tag_id, :taggable_id, :taggable_type#, :tag_context
 
-  def taggable
-    eval("#{taggable_type}.get!(#{taggable_id})") if taggable_type and taggable_id
-  end
+#  def taggable
+#    eval("#{taggable_type}.get!(#{taggable_id})") if taggable_type and taggable_id
+#  end
 end
