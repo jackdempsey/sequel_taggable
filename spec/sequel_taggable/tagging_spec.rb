8ce01fd97b5861eca7bc9c8b84f7e4fc818e6319
Jacob Dunphy <jacob.dunphy@gmail.com>
2009-01-20 19:47:26 -0800

    Trying to clarify the tagging specs a bit, and moving the currently commented out functionality into a pending spec.

--- spec/sequel_taggable/tagging_spec.rb
+++ spec/sequel_taggable/tagging_spec.rb
@@ -2,27 +2,31 @@ require File.dirname(__FILE__) + '/../spec_helper.rb'
 
 describe Tagging do
   before(:each) do
-    @tagging = Tagging.new
+    @tagging = Tagging.new(:tag_id => 1, :taggable_id => 1, :taggable_type => "TaggedModel")
   end
 
-  it "should have properties: id, tag_id, taggable_id, taggable_type, tagger_id, tagger_type, and tag_context" do
+  it "should have properties: id, tag_id, taggable_id, taggable_type, taggable_id and taggable_type" do
     @tagging.should respond_to(:id)
     @tagging.should respond_to(:tag_id)
     @tagging.should respond_to(:taggable_id)
     @tagging.should respond_to(:taggable_type)
-    @tagging.should respond_to(:tag_context)
   end
 
-  it "should validate the presence of tag_id, taggable_id, taggable_type and tag_context" do
-    @tagging.should_not be_valid
-    @tagging.tag_id = 1
-    @tagging.should_not be_valid
-    @tagging.taggable_id = 1
-    @tagging.should_not be_valid
-    @tagging.taggable_type = "TaggedModel"
-    @tagging.should_not be_valid
-    @tagging.tag_context = "skills"
+  it "should validate the presence of tag_id, taggable_id and taggable_type" do
     @tagging.should be_valid
+    
+    Tagging.new(:taggable_id => 1, :taggable_type => "TaggedModel").should_not be_valid
+
+    Tagging.new(:tag_id => 1, :taggable_type => "TaggedModel").should_not be_valid
+
+    Tagging.new(:tag_id => 1, :taggable_id => 1).should_not be_valid   
+  end
+  
+  it "should have and require and tag_context" do
+    pending
+    @tagging.should respond_to(:tag_context)
+    @tagging.tag_context =  nil
+    @tagging.should_not be_valid
   end
 
   it "should many_to_one :tag" do
